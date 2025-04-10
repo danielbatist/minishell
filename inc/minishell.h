3f84fd128a58866b440e10b826612fd0346432e5
eteofilo <eli.teofilo75@gmail.com>
2025-04-10 13:50:19 -0300

    Adding header file

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: eteofilo <eteofilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 16:50:41 by dbatista          #+#    #+#             */
-/*   Updated: 2025/04/08 16:48:35 by eteofilo         ###   ########.fr       */
+/*   Updated: 2025/04/10 13:49:22 by eteofilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,18 @@
 # include "../lib/libft.h"
 
 typedef enum e_token_type {
+	COMMAND,
+	FLAG,
 	PIPE,
 	REDIRECT_IN,
 	REDIRECT_OUT,
 	APPEND,
 	HEREDOC,
-	COMMAND,
 	PARAMETER,
-	SINGLE_QUOTED,
-	DOUBLE_QUOTED,
+	SINGLE_QUOTED = 39,
+	DOUBLE_QUOTED = 34,
 	UNCLOSED,
+	EOF_TOKEN
 }	t_token_type;
 
 typedef struct s_token
@@ -37,6 +39,15 @@ typedef struct s_token
 	t_token_type	type;
 	char			*lexeme;
 }	t_token;
+typedef struct s_scanner
+{
+	int	start;
+	int	current;
+	int	line;
+	char	*src;
+	t_list	*tokens;
+}	t_scanner;
+
 
 t_list	*tokenizer(char *input);
 

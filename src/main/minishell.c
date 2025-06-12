cdf2c9a38d6b715d61198da6b54373adeba2eaeb
eteofilo <eli.teofilo75@gmail.com>
2025-06-11 22:33:05 -0300

    put the minishell design

--- src/main/minishell.c
+++ src/main/minishell.c
@@ -3,15 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dbatista <dbatista@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eteofilo <eteofilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 13:34:40 by eteofilo          #+#    #+#             */
-/*   Updated: 2025/06/11 18:02:56 by dbatista         ###   ########.fr       */
+/*   Updated: 2025/06/11 22:32:14 by eteofilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
+
+static void	print_pwd(void)
+{
+	int		i;
+	char	*pwd;
+	i = 0;
+	pwd = getcwd(NULL, 0);
+	if (ft_strncmp(pwd, "/home/eteofilo/", ft_strlen(pwd)) == 0)
+	{
+		free(pwd);
+		pwd = ft_strdup("~");
+	}
+	else
+		i = 15;
+	set_signal_readline();
+	ft_printf_fd(1, "\033[38;2;173;255;47m\001\033[1m\002"
+		"%s"
+		"\001\033[0m\002\n", pwd + i);
+	free(pwd);
+}
 int	main(int ac, char **av, char **envp)
 {
 	char		*input;
@@ -22,8 +42,10 @@ int	main(int ac, char **av, char **envp)
 	env_list = catch_env(envp);
 	while (1)
 	{
-		set_signal_readline();
-		input = readline("minishell> ");
+		print_pwd();
+		input = readline("\033[38;2;135;206;250m\001\033[1m\002"
+			">"
+			"\001\033[0m\002 ");
 		if (!input)
 			handle_exit(NULL, env_list);
 		set_signal_exec_parent();
